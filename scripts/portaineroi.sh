17501a7cb600641e65cb46c449f17f230bc043f3
josephsmartinez <josephsmartinez@gmail.com>
2019-02-11 17:35:25 -0500

    portainer sh

---
+++ scripts/portaineroi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker volume create portainer_data
+
+# Run Portainer.io when needed
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
